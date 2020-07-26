4deb8aa5bbf2c45792d5b76a12dfa4c97107f67e
Ahmed Al-kharusi <63564296+Ahmed-alkharusi@users.noreply.github.com>
2020-07-26 09:48:16 +0400

    added the Julia set

--- mandelbrot set (C++ wtih Python plots)/mandelbrot set/Source.cpp
+++ mandelbrot set (C++ wtih Python plots)/mandelbrot set/Source.cpp
@@ -1,7 +1,7 @@
 /*
-====================
-=The Mandelbrot set=
-====================
+============================
+=Mandelbrot and Julia sets=
+============================
 Created on Sat Jul 25 2020
 
 @author : Ahmed Al-kharusi*/
@@ -14,10 +14,19 @@ Created on Sat Jul 25 2020
 #include <string>
 #include <cmath>
 int mandelbrot(Complex domain, size_t range);
+int julia(Complex zo, Complex domain, size_t range);
 void output_matrix(std::vector<std::vector<double>> matrix, std::string&& file_name);
 
 int main() {
-	
+	/*some points for the Julia set
+	const Complex c{-1,0.1};
+	const Complex c1{ 0.61803398875,0 };
+	const Complex c2{ -0.4,0.6 };
+	const Complex c3{ 0.285,0.01 };
+	const Complex c4{ -0.8,0.156 };
+	const Complex c5{ -0.835,- 0.2321 };
+	const Complex c6{ 0,0.8 };
+	*/
 	double points_no{ 2000 };
 	std::vector<std::vector<double>> result(points_no, std::vector<double>(points_no, 0));
 	double x_start{ -2 };
@@ -27,7 +36,7 @@ int main() {
 		for (size_t j{ 0 };j < points_no;j++) {
 			y_start += 3  / points_no;
 			Complex temp{ x_start,y_start };
-			result[j][i] = mandelbrot(temp, 100);
+			result[j][i] = mandelbrot(temp, 100);//change this to julia(temp, c, 100) for the Julia set
 		}	
 	}
 	output_matrix(result, "results.txt");
@@ -41,6 +50,18 @@ int mandelbrot(Complex domain, size_t range) {
 		result = (zo * zo) + domain;
 		zo = result;
 
+		double ans = result.re() * result.re() + result.im() * result.im();
+		if (ans >= 4)
+			return i;
+	}
+	return 0;
+}
+int julia(Complex zo, Complex domain, size_t range) {
+	
+	Complex result{ 0,0 };
+	for (size_t i{ 0 };i < range;i++) {
+		result = (zo * zo) + domain;
+		zo = result;
 		double ans = result.re() * result.re() + result.im() * result.im();
 		if (ans >= 4)
 			return i;
