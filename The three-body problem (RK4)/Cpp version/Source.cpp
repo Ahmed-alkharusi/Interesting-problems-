4c1cc723062782f013638610c8b444c173b1dba3
Ahmed Al-kharusi <63564296+Ahmed-alkharusi@users.noreply.github.com>
2020-08-01 12:28:36 +0400

    optimized the code

--- The three-body problem (RK4)/Cpp version/Source.cpp
+++ The three-body problem (RK4)/Cpp version/Source.cpp
@@ -15,9 +15,9 @@ Created on Jul 30 2020
 
 
 void output_matrix(std::vector < std::vector<std::vector<double>>> &result_obj, std::string&& file_name);
-Matrix deriv(Matrix obj_active, Matrix obj_passive1, double mp1, Matrix obj_passive2, double mp2);
-void rk4(Matrix(*deriv)(Matrix, Matrix,double, Matrix, double), Matrix &obj_active, Matrix &obj_passive1, double mp1, Matrix &obj_passive2, double mp2, const double h);
-void implement_rk4(Matrix(*deriv)(Matrix, Matrix, double, Matrix, double),Matrix &obj_1,double &m1,Matrix &obj_2, double m2, Matrix &obj_3, double m3,double t, double step_size,double steps_no, std::vector<std::vector<std::vector<double>>> &result_obj1, std::vector<std::vector<std::vector<double>>> &result_obj2, std::vector<std::vector<std::vector<double>>> &result_obj3);
+Matrix deriv(Matrix obj_active, Matrix obj_passive1, const double& mp1, Matrix obj_passive2, const double& mp2);
+void rk4(Matrix(*deriv)(Matrix, Matrix,const double&, Matrix, const double&), Matrix &obj_active, Matrix &obj_passive1,const double &mp1, Matrix &obj_passive2,const double &mp2, const double &h);
+void implement_rk4(Matrix(*deriv)(Matrix, Matrix, const double&, Matrix, const double&),Matrix &obj_1,const double &m1,Matrix &obj_2,const double &m2, Matrix &obj_3, const double &m3,double &t, const double &step_size, const long unsigned int &steps_no, std::vector<std::vector<std::vector<double>>> &result_obj1, std::vector<std::vector<std::vector<double>>> &result_obj2, std::vector<std::vector<std::vector<double>>> &result_obj3);
 
 int main() {
 	auto start = std::chrono::high_resolution_clock::now();
@@ -25,20 +25,20 @@ int main() {
 	const long unsigned int steps_no{ 100000 };//number of steps of the RK4 method
 	//initial conditions (play around with these)
 	double t{ 0 };
-	double m1{ 1 };
-	double m2{ 1 };
-	double m3{ 1 };
+	const double m1{ 1 };
+	const double m2{ 1 };
+	const double m3{ 1 };
 
-	Matrix obj_1{ Array{0,0},  Array{0,0} };
+	/*Matrix obj_1{ Array{0,0},  Array{0,0} };
 	Matrix obj_2{ Array{1.5*pow(2.0,0.5),0}, Array{-0.5,0} };
-	Matrix obj_3{ Array{pow(2.0,0.5)/2,0}, Array{3*pow(3.0/2.0,0.5),0} };
+	Matrix obj_3{ Array{pow(2.0,0.5)/2,0}, Array{3*pow(3.0/2.0,0.5),0} };*/
 
-	//double v1{ 0.213841083 };
-	//double v2{ 0.054293840 };
-	//Matrix obj_1 {Array{-1,v1},  Array{0,v2}};
-	//Matrix obj_2 {Array{1,v1}, Array{0,v2}};
-	//Matrix obj_3 {Array{0,-2*m1*v1/m3 },Array{0,  -2*m1 * v2/m3 }};
-
+	double v1{ 0.213841083 };
+	double v2{ 0.054293840 };
+	Matrix obj_1 {Array{-1,v1},  Array{0,v2}};
+	Matrix obj_2 {Array{1,v1}, Array{0,v2}};
+	Matrix obj_3 {Array{0,-2*m1*v1/m3 },Array{0,  -2*m1 * v2/m3 }};
+	
 	//result matrices
 	std::vector<std::vector<std::vector<double>>> result_obj1;
 	std::vector<std::vector<std::vector<double>>> result_obj2;
@@ -47,7 +47,6 @@ int main() {
 	//implementation
 	implement_rk4(deriv, obj_1, m1, obj_2, m2, obj_3, m3, t, step_size, steps_no, result_obj1, result_obj2, result_obj3);
 	std::cout << "Writing files ... \n";
-
 
 	output_matrix(result_obj1, "result_obj1.txt");
 	output_matrix(result_obj2, "result_obj2.txt");
@@ -58,7 +57,7 @@ int main() {
 	
 	return 0;
 }
-Matrix deriv(Matrix obj_active, Matrix obj_passive1, double mp1, Matrix obj_passive2, double mp2) {
+Matrix deriv(Matrix obj_active, Matrix obj_passive1, const double &mp1, Matrix obj_passive2,const double &mp2) {
 	double v_x = (obj_active.arr_x).speed;
 	double v_y = (obj_active.arr_y).speed;
 	double xo = (obj_active.arr_x).position;
@@ -73,14 +72,14 @@ Matrix deriv(Matrix obj_active, Matrix obj_passive1, double mp1, Matrix obj_pass
 
 	return Matrix{ Array{v_x,acc_x}, Array{v_y,acc_y} };
 }
-void rk4(Matrix(*deriv)(Matrix, Matrix, double, Matrix, double), Matrix& obj_active, Matrix& obj_passive1, double mp1, Matrix& obj_passive2, double mp2, const double h) {
+void rk4(Matrix(*deriv)(Matrix, Matrix,const double&, Matrix,const double&), Matrix& obj_active, Matrix& obj_passive1, const double &mp1, Matrix& obj_passive2,const double &mp2, const double &h) {
 	Matrix k1 = deriv( obj_active        , obj_passive1, mp1,  obj_passive2,  mp2);
 	Matrix k2 = deriv( obj_active + h / 2, obj_passive1, mp1,  obj_passive2,  mp2);
 	Matrix k3 = deriv( obj_active + h / 2, obj_passive1, mp1,  obj_passive2,  mp2);
 	Matrix k4 = deriv( obj_active + h    , obj_passive1, mp1,  obj_passive2,  mp2);
 	obj_active = obj_active + ((k1 + (k2 * 2) + (k3 * 2) + k4)) * (h / 6);
 }
-void implement_rk4(Matrix(*deriv)(Matrix, Matrix, double, Matrix, double), Matrix& obj_1, double& m1, Matrix& obj_2, double m2, Matrix& obj_3, double m3, double t, double step_size, double steps_no, std::vector<std::vector<std::vector<double>>>& result_obj1, std::vector<std::vector<std::vector<double>>>& result_obj2, std::vector<std::vector<std::vector<double>>>& result_obj3) {
+void implement_rk4(Matrix(*deriv)(Matrix, Matrix,const double&, Matrix,const double&), Matrix &obj_1, const double& m1, Matrix &obj_2, const double &m2, Matrix &obj_3, const double &m3, double &t, const double &step_size, const long unsigned int &steps_no, std::vector<std::vector<std::vector<double>>>& result_obj1, std::vector<std::vector<std::vector<double>>>& result_obj2, std::vector<std::vector<std::vector<double>>>& result_obj3) {
 	//std::vector<double> time_arr; implement this to calculate the time arr
 	std::vector<double> obj_1_x{ {(obj_1.arr_x).position},{(obj_1.arr_x).speed} };
 	std::vector<double> obj_2_x{ {(obj_2.arr_x).position},{(obj_2.arr_x).speed} };
